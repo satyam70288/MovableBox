407e82ffd9a7a41c39242635b7fbe70fdb6008ce
satyam70288 <satyamb971@gmail.com>
2023-09-27 15:53:53 +0530

    move

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState,useRef,useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -13,12 +13,36 @@ function App() {
 function DraggableBox() {
   const [isDragging, setIsDragging] = useState(false);
   const [offset, setOffset] = useState({ x: 0, y: 0 });
-  const [position, setPosition] = useState({ x: 200, y: 100 });
+  const [position, setPosition] = useState({ x:0 , y: 0 });
+  const [divWidth, setDivWidth] = useState(null);
+  const [divheight, setDivHeight] = useState(null)
+  const divRef = useRef(null);
 
+  // const divElement = document.getElementsByClassName('draggable-box');
+  useEffect(() => {
+    // Function to update the div width
+    const updateDivWidth = () => {
+      if (divRef.current) {
+        setDivWidth(divRef.current.offsetWidth);
+        setDivHeight(divRef.current.offsetheight);
+      }
+    };
+    updateDivWidth();
+
+    // Attach a resize event listener to update the width when the window is resized
+    window.addEventListener('resize', updateDivWidth);
+
+    // Cleanup by removing the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', updateDivWidth);
+    };
+  }, [divWidth]);
   const handleMouseDown = (e) => {
     setIsDragging(true);
     console.log(window.innerWidth)
     console.log(window.innerHeight)
+    console.log("width" + divWidth)
+    console.log("height" + divheight)
     setOffset({
       x: e.clientX - position.x,
       y: e.clientY - position.y,
@@ -43,6 +67,7 @@ function DraggableBox() {
       newY=0;
       }
     }
+
   //y0axis with x0 axis
     if (newY < 100) {
       if(newX < 100){
@@ -51,21 +76,22 @@ function DraggableBox() {
       }
     }
     // x innerwith
-    else if (newX+100   > window.innerWidth-900) {
-      newX= window.innerWidth-800;
+    else if (newX  > window.innerWidth-divWidth+100) {
+      newX= window.innerWidth-divWidth;
     }
+
     // x innerwith with y innerHeight
-     if (newX+100   > window.innerWidth-900) {
+     if (newX+100   > window.innerWidth-divWidth) {
       if(newY > window.innerHeight-700){
-      newX= window.innerWidth-800;
+      newX= window.innerWidth-divWidth;
       newY = window.innerHeight - 600;
       console.log(window.innerWidth)
       }
     }
     // y0 axis with x inner width
     if (newY < 100) {
-      if(newX  > window.innerWidth-900) {
-        newX= window.innerWidth-800;
+      if(newX  > window.innerWidth-divWidth+100) {
+        newX= window.innerWidth-divWidth;
       
       // newX = 0;
       newY=0;
@@ -75,31 +101,13 @@ function DraggableBox() {
       
       newY = 0;
     }
-   
-    // }
     
     
-    // // else if (newY>400) {
-    // //   newY = 400;
-    // // }
-
-    else if (newY > window.innerHeight-700) {
+    else if (newY > window.innerHeight-650) {
       newY = window.innerHeight - 600;
     }
 
-    // else if (newX > 200) {
-    //   newX = 200;
-    // }
-    // else if (newY>100 && newX<100) {
-    //   newX = 0;
-    //   newY = 0;
-    // }
-    // else if(newX >window.innerWidth-900 && newY >window.innerHeight-700){
-    //   newX=window.innerWidth-800;
-    //   newY=window.innerHeight-600
-    // }
-     
-    setPosition({
+        setPosition({
       x: newX,
       y: newY
       
@@ -114,11 +122,17 @@ function DraggableBox() {
   return (
     <div
       className={`draggable-box ${isDragging ? 'dragging' : ''}`}
-      style={{ left: position.x, top: position.y }}
+       style={{ left: position.x, top: position.y }}
+       ref={divRef}
+
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
-    ><div className='box'>
+    ><div className='box' 
+    // onMouseMove={handleMouseMove}
+    // onMouseDown={handleMouseDown}
+    // onMouseUp={handleMouseUp}
+    >
       Drag me!
     </div>
       
